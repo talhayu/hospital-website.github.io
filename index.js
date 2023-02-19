b2405bf4d818c8f353390732221e8568753694a8
talhayu <mnhetz999@gmail.com>
2023-02-19 15:50:35 +0500

    done

--- index.js
+++ index.js
@@ -18,10 +18,10 @@ window.onscroll = () =>{
     }
   }
 
-  function loader(){
-    document.querySelector('.loader-container').classList.add('fade-out')
-  }
-  function fadeout(){
-    setInterval(loader, 3000)
-  }
-  window.onload = fadeout();
+  // function loader(){
+  //   document.querySelector('.loader-container').classList.add('fade-out')
+  // }
+  // function fadeout(){
+  //   setInterval(loader, 3000)
+  // }
+  // window.onload = fadeout();
